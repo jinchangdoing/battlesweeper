ea90fc8a480bf97ea076537a79e783b6376f8922
auuwa-star-13 <auuwako@gmail.com>
2022-09-24 21:44:42 +0800

    Signed-off-by: auuwa-star-13 <auuwako@gmail.com>

--- index.js
+++ index.js
@@ -17,15 +17,16 @@
         mineExp: [],
         mineFlaged:[],
     };
-    var firstflag=true;
+    var firstflag = true;
     var mineList = [];
-    var $app = document.querySelector('#app');
-    var $actions = document.querySelector('.actions');
-    var $box = document.querySelector('.box');
-    var $info = document.querySelector('.info');
-    var $main = document.querySelector('.main');
-    var $toolbar = document.querySelector('.toolbar');
-    var $toolbarWrapper = document.querySelector('.toolbar-wrapper');
+    var $app = $('#app');
+    var $actions = $('.actions');
+    var $box = $('.box');
+    var $boxWrapper = $('.box-wrapper');
+    var $info = $('.info');
+    var $main = $('.main');
+    var $toolbar = $('.toolbar');
+    var $toolbarWrapper = $('.toolbar-wrapper');
     function getPlayerLevel() {
         var level = 0;
         _.forEach(playerInfo.exprequire, function(exp, i) {
@@ -37,9 +38,8 @@
     }
     function getNextLevelExp() {
         var nextExp = 0;
-        
-        nextExp=playerInfo.exprequire[getPlayerLevel()]-playerInfo.exp
-        if(nextExp>10000)
+        nextExp = playerInfo.exprequire[getPlayerLevel()]-playerInfo.exp
+        if(nextExp > 10000)
             return '-';
         return nextExp
     }
@@ -47,42 +47,42 @@
 
 
     // 点击事件
-    function blockClick(domMask) {
-        if(domMask.classList.value !== 'block-mask') {
+    function blockClick(i) {
+        // 获取$dom对象
+        var $block = $(`.block[index="${i}"]`);
+        var $blockMask = $block.find('.block-mask');
+        if(!$blockMask[0]) {
             return;
         }
+        // 获取全局变量
         var height = gameInfo.height;
-        var block = domMask.parentElement;
-        var i = parseInt(block.attributes.index.value);
         var mine = mineList[i];
         
        //扣除标记数
-       var blockFlag = document.querySelector(`.block[index="${block.attributes.index.value}"] > .block-flag`);
-       if(block.attributes.flag && $block.attributes.flag.value) {
-           var a = parseInt(block.attributes.flag.value);  
+       if($block.attr('flag')) {
+           var a = parseInt($block.attr('flag'));  
            gameInfo.mineFlaged[a - 1]--;
            updateInfo();
+           $block.removeAttr('flag');
        }
-       block.removeAttribute('flag');
-       blockFlag.innerText = '';
-
-       block.innerHTML = `<div class="block-${mine.type} num-${mine.number}">
-       ${mine.number ? mine.number : ''}
-       </div>`;
        
        // 第一次点击必白
        if((mine.type !== 'space' || mine.number !== 0) && firstflag) {
             initData();
             initView();
-            
             setTimeout(function() { 
-                var target = document.querySelector(`.block[index="${i}"] > .block-mask`);
-                blockClick(target);
+                blockClick(i);
             },0);
             return;    
-                    
        }
        firstflag = false;
+
+       // 打开雷
+       $block.html(`
+       <div class="block-${mine.type} num-${mine.number}">
+        ${mine.number ? mine.number : ''}
+       </div>
+       `);
 
        // 点击到空白时 点击周围八格
        if(mine.type === 'space' && mine.number === 0) {
@@ -90,37 +90,35 @@
             for(var n = 0; n < 9; n++) {
                 var x = parseInt(n / 3) - 1;
                 var y = parseInt(n % 3) - 1;
-                if(i % height === 0 && x === -1){
+                if(i % height === 0 && x === -1) {
                    continue
                 }
                 if(i % height === (height - 1) && x === 1){
                    continue
                 }
                 var index = i + x + y * height;
-                
-            var dom = document.querySelector(`.block[index="${index}"] > .block-mask`);
-            if(dom) blockClick(dom);
-        }
+                blockClick(index);
+            }
         }, 10);
        }
        // 点击到雷时 扣除血量
        if(mine.type === 'mine' && mine.number > getPlayerLevel()) {
             playerInfo.hp -= mine.number * (mine.number - getPlayerLevel());
-            $box.style.transform='rotate(3deg)'
+            $box.css('transform', 'rotate(3deg)');
             setTimeout(function(){
-                $box.style.transform='rotate(-3deg)'
+                $box.css('transform', 'rotate(-3deg)');
             },50)
             setTimeout(function(){
-                $box.style.transform='rotate(3deg)'
+                $box.css('transform', 'rotate(3deg)');
             },150)
             setTimeout(function(){
-                $box.style.transform='rotate(-3deg)'
+                $box.css('transform', 'rotate(-3deg)');
             },200)
             setTimeout(function(){
-                $box.style.transform='rotate(3deg)'
+                $box.css('transform', 'rotate(3deg)');
             },250)
             setTimeout(function(){
-                $box.style.transform='rotate(0deg)'
+                $box.css('transform', 'rotate(0deg)');
             },300)
        }
        // 血量归零时 游戏结束
@@ -132,60 +130,55 @@
             gameInfo.mineNum[mine.number-1]--;
             playerInfo.exp += gameInfo.mineExp[mine.number-1];
        }
-
-       // 展示数据
+       // 更新展示数据
        updateInfo();
     }
     
+    // 消息提示
     function message(msg) {
-        var message = document.createElement('div');
-        message.classList.add('message');
-        message.innerText = msg;
-        message.style.top = '-100px';
-        message.style.opacity = '0';
-        $app.appendChild(message);
+        var $message = $(`<div class="message" style="top: -100px; opacity: 0;">
+            ${msg}
+        </div>`);
+        $app.append($message);
         setTimeout(function() {
-            message.style.top = '140px';
-            message.style.opacity = '1';
+            $message.css('top', '140px');
+            $message.css('opacity', '1');
         }, 0);
         setTimeout(function() {
-            message.style.top = '-100px';
-            message.style.opacity = '0';
+            $message.css('top', '-100px');
+            $message.css('opacity', '0');
         }, 2000);
         setTimeout(function() {
-            $app.removeChild(message);
+            $app.remove($message);
         }, 3000);
     }
 
     function gameOver() {
         message('你输了 - ^ -');
-        // 取消点击事件
-        var doms = document.querySelectorAll(`.block > .block-mask`);
         // 结束计时器
-        if(inter)
+        if(inter) {
             clearInterval(inter);
-        
-        
-        _.forEach(doms, function(d, i){
-            d.style.pointerEvents = 'none';
-        })
+        }
+        // 取消点击事件
+        $(`.block > .block-mask`).each(function(i, d) {
+            $(d).css('pointer-events', 'none');
+        });
         // 展示雷的位置
         _.filter(mineList, function(m) {
             return m.type === 'mine';
         }).forEach(function(m) {
-            var dom = document.querySelector(`.block[index="${m.i}"] > .block-show`);
+            var $dom = $(`.block[index="${m.i}"] > .block-show`);
             setTimeout(function() {
-                if(dom) {
-                    dom.style.display = 'block';
-                    dom.innerText = m.number;
+                if($dom[0]) {
+                    $dom.css('display', 'block');
+                    $dom.html(m.number);
                 }
             }, 1 * m.i);
         });
     }
 
     function updateInfo() {
-           
-        $info.innerHTML = `
+        $info.html(`
             <table cellspacing=0>
                 <tr>
                     <th>雷等级</th>
@@ -238,7 +231,7 @@
                     <td colspan="2" class="time">${time}</td>
                 </tr>    
             </table>
-            `;
+            `);
     }
 
     // 数据初始化
@@ -261,7 +254,7 @@
         mineList = _.concat(mineList, _.fill(new Array(num - mineList.length), space));
         // 打乱顺序
         mineList = _.sortBy(mineList, function() {
-            return Math.random()
+            return Math.random();
         });
         // 计算坐标
         _.forEach(mineList, function(m, i) {
@@ -297,33 +290,24 @@
     // 页面初始化
     function initView() {
         var height = gameInfo.height;
-        var $blocks = document.createElement('div');
-        $box.innerHTML = '';
-        $blocks.classList.add('blocks');
+        var $blocks = $(`<div class="blocks"></div>`);
+        $box.html('');
         _.forEach(mineList, function(m, i) {
             if(m.y === 0) {
-                $box.appendChild($blocks);
+                $box.append($blocks);
             }
-            var $block = document.createElement('div');
-            var $blockMask = document.createElement('div');
-            var $blockFlag = document.createElement('div');
-            var $blockShow = document.createElement('div');
-            $block.classList.add('block');
-            $blockMask.classList.add('block-mask');
-            $blockFlag.classList.add('block-flag');
-            $blockShow.classList.add('block-show');
-            $block.setAttribute('index', i);
-            $block.appendChild($blockMask);
-            $block.appendChild($blockFlag);
-            $block.appendChild($blockShow);
-            $blocks.appendChild($block);
+            var $block = $(`<div class="block" index="${i}">
+            <div class="block-mask purple-body"></div>
+            <div class="block-show red-body"></div>
+            <div class="block-flag"></div>
+            </div>`);
+            $blocks.append($block);
             if(m.y === height - 1) {
-                $blocks = document.createElement('div');
-                $blocks.classList.add('blocks');
+                $blocks = $(`<div class="blocks"></div>`);
             }
         });
   
-        $toolbar.innerHTML=`    
+        $toolbar.html(`    
                 <button class="flag clearflag" data="0">0</button>
                 <button class="flag setflag" data="1">1</button> 
                 <button class="flag setflag" data="2">2</button> 
@@ -334,61 +318,54 @@
                 <button class="flag setflag" data="7">7</button> 
                 <button class="flag setflag" data="8">8</button> 
                 <button class="flag setflag" data="9">9</button>      
-        `;
+        `);
         
-        // 增加右键事件
+        // 右键事件
         var targetMask;
-        var $mines = document.querySelectorAll(`.block > .block-mask`);
-        _.forEach($mines, function(m, i) {
-            m.addEventListener('contextmenu', function(e) {
-                targetMask = e.target;
-                $toolbar.style.left = e.clientX + 'px';
-                $toolbar.style.top = e.clientY + 'px';
-                $toolbarWrapper.style.display = 'block';
-            });
-        })
+        $(`.block > .block-mask`).on('contextmenu', function(e) {
+            targetMask = e.target;
+            $toolbar.css('left', e.clientX + 'px');
+            $toolbar.css('top', e.clientY + 'px');
+            $toolbarWrapper.css('display', 'block');
+        });
 
         // 清除雷等级
-        var $clearflag = document.querySelector('.clearflag');
-        $clearflag.addEventListener('click', function(e) {
+        $('.clearflag').on('click', function(e) {
             if(!targetMask) {
                 return;
             }
-            var block = targetMask.parentElement;
-            var blockFlag = document.querySelector(`.block[index="${block.attributes.index.value}"] > .block-flag`);
+            var $block = $(targetMask.parentElement);
+            var $blockFlag = $(`.block[index="${block.attributes.index.value}"] > .block-flag`);
             // 减去原有标记的等级
-            if(block.attributes.flag && block.attributes.flag.value) {
-                var a = parseInt(block.attributes.flag.value);  
+            if($block.attr('flag')) {
+                var a = parseInt($block.attr('flag'));  
                 gameInfo.mineFlaged[a - 1]--;
                 updateInfo();
             }
             // 取消标记等级
-            block.removeAttribute('flag');
-            blockFlag.innerText = '';
+            $block.removeAttr('flag');
+            $blockFlag.html('');
         });
         
         // 标记雷等级
-        var $setflags = document.querySelectorAll(`.setflag`);
-        _.forEach($setflags,function(s, i) {
-            s.addEventListener('click', function(e) {     
-                var flagNum = parseInt(e.target.attributes.data.value);   
-                if(!targetMask) {
-                    return;
-                }
-                var block = targetMask.parentElement;
-                var blockFlag = document.querySelector(`.block[index="${block.attributes.index.value}"] > .block-flag`);
-                // 减去原有标记的等级
-                if(block.attributes.flag && block.attributes.flag.value) {
-                    var a = parseInt(block.attributes.flag.value);  
-                    gameInfo.mineFlaged[a - 1]--;
-                }
-                // 标记新等级
-                block.setAttribute('flag', flagNum);
-                blockFlag.innerText = flagNum;
-                gameInfo.mineFlaged[flagNum - 1]++;
-                updateInfo();        
-            });
-        })
+        $(`.setflag`).on('click', function(e) {
+            if(!targetMask) {
+                return;
+            }
+            var flagNum = $(e.target).attr("data");
+            var $block = $(targetMask.parentElement);
+            var $blockFlag = $(`.block[index="${$block.attr('index')}"] > .block-flag`);
+            // 减去原有标记的等级
+            if(flagNum) {
+                var a = parseInt(flagNum);  
+                gameInfo.mineFlaged[a - 1]--;
+            }
+            // 标记新等级
+            $block.attr('flag', flagNum);
+            $blockFlag.html(flagNum);
+            gameInfo.mineFlaged[flagNum - 1]++;
+            updateInfo();        
+        });
         // 展示数据
         updateInfo();
     }
@@ -396,13 +373,13 @@
     function init(diff) {
         difficulty = diff;
         $actions.innerHTML = '';
-        $actions.appendChild(createButton('easy', '初级'));
-        $actions.appendChild(createButton('normal', '中级'));
-        $actions.appendChild(createButton('hard', '高级'));
-        $actions.appendChild(createButton('lunatic', '疯狂'));
-        $actions.appendChild(createButton('extra', '鱼'));
-        $actions.appendChild(createButtonZoom('up', '放大'));
-        $actions.appendChild(createButtonZoom('down', '缩小'));
+        $actions.append(createButton('easy', '初级'));
+        $actions.append(createButton('normal', '中级'));
+        $actions.append(createButton('hard', '高级'));
+        $actions.append(createButton('lunatic', '疯狂'));
+        $actions.append(createButton('extra', '鱼'));
+        $actions.append(createButtonZoom('up', '放大'));
+        $actions.append(createButtonZoom('down', '缩小'));
         initData(diff);
         initView();
         time = 0;
@@ -417,25 +394,23 @@
 
         message(`游戏开始, 等级${diff}`);
         function createButton(level, text) {
-            var $button = document.createElement('button');
-            $button.classList.add('actions-button');
-            $button.innerText = text;
-            $button.addEventListener('click', function(e) {
+            var $button = $(`<button class="actions-button purple-body">${text}</button>`);
+            $button.on('click', function(e) {
                 init(level);
             });
             return $button;
         };
 
         function createButtonZoom(up, text) {
-            var $button = document.createElement('button');
-            $button.classList.add('actions-button');
-            $button.innerText = text;
-            $button.addEventListener('click', function(e) {
+            var $button = $(`<button class="actions-button purple-body">${text}</button>`);
+            $button.on('click', function(e) {
                 if(up === 'up') {
-                    $main.style.transform = `scale(${zoomList[++zoomLevel]})`;
+                    $boxWrapper.css('transform',`scale(${zoomList[++zoomLevel]})`);
                 } else {
-                    $main.style.transform = `scale(${zoomList[--zoomLevel]})`;
+                    $boxWrapper.css('transform',`scale(${zoomList[--zoomLevel]})`);
                 }
+                if(zoomLevel < 0 ) zoomLevel = 0;
+                if(zoomLevel >= zoomList.length) zoomLevel = zoomList.length - 1;
             });
             return $button;
         }
@@ -448,43 +423,52 @@
     // 缩放事件
     var zoomList = [0.1, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.5, 2.0];
     var zoomLevel = 5;
-    window.addEventListener('wheel', function(e) {
+    $(window).on('wheel', function(e) {
         if(e.deltaY > 0) {
-            $main.style.transform = `scale(${zoomList[--zoomLevel]})`;
+            $boxWrapper.css('transform',`scale(${zoomList[--zoomLevel]})`);
         } else {
-            $main.style.transform = `scale(${zoomList[++zoomLevel]})`;
+            $boxWrapper.css('transform',`scale(${zoomList[++zoomLevel]})`);
         }
+        if(zoomLevel < 0 ) zoomLevel = 0;
+        if(zoomLevel >= zoomList.length) zoomLevel = zoomList.length - 1;
     });
 
     // 鼠标拖拽事件
     var cx = 0, cy = 0, mx = 0, my = 0;
-    window.addEventListener('mousedown', function(e) {
+    $main.on('mousedown', function(e) {
         if(e.button !== 0) {
             return;
         }
         cx = e.clientX;
         cy = e.clientY;
-        window.addEventListener('mousemove', onMouseMove);
+        $main.on('mousemove', onMouseMove);
     });
-    window.addEventListener('mouseup', function(e) {
+    $main.on('mouseup', function(e) {
         if(e.button !== 0) {
             return;
         }
-        if (Math.abs(e.clientX - cx) < 5 && Math.abs(e.clientY - cy < 5)) {
-            blockClick(e.target);
-        }
         mx = mx - cx + e.clientX;
         my = my - cy + e.clientY;
-        window.removeEventListener('mousemove', onMouseMove);
+        $main.off('mousemove', onMouseMove);
+
+        if(!$(e.target).is('.block-mask')) {
+            return;
+        }
+        if (Math.abs(e.clientX - cx) > 5 || Math.abs(e.clientY - cy > 5)) {
+            return;
+        }
+        var index = $(e.target.parentElement).attr('index');
+        blockClick(parseInt(index));
     });
+
     function onMouseMove(e) {
-        $main.style.left = `${mx - cx + e.clientX}px`;
-        $main.style.top = `${my - cy + e.clientY}px`;
+        $box.css('left', `${mx - cx + e.clientX}px`);
+        $box.css('top', `${my - cy + e.clientY}px`);
     }
 
     // 右键工具栏点击关闭事件
-    $toolbarWrapper.addEventListener('click', function(e) {
-        $toolbarWrapper.style.display = 'none';
+    $toolbarWrapper.on('click', function(e) {
+        $toolbarWrapper.css('display', 'none');
 });
 
     
